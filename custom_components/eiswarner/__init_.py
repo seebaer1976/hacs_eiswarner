0d2d00114f350ac3eb2398805a3cd9ea3eaad5c9
seebaer1976 <seebaer1976@web.de>
2023-12-10 22:02:26 +0100

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	custom_components/eiswarner/manifest.json

--- custom_components/eiswarner/__init_.py
+++ custom_components/eiswarner/__init_.py
@@ -1,14 +1,17 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from .config_flow import EiswarnerConfigFlow
+from config_flow import EiswarnerConfigFlow  # Korrigierter Import
 
 DOMAIN = "eiswarner"
+
 
 async def async_setup(hass, config):
     return True
 
+
 async def async_setup_entry(hass, entry: ConfigEntry):
     return True
+
 
 async def async_get_config_flow(hass, entry):
     return EiswarnerConfigFlow(hass, entry)
