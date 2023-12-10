31771ec98a057117ee6baf4cbcf59ec574008cc5
seebaer1976 <seebaer1976@web.de>
2023-12-10 18:33:56 +0100

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	custom_components/eiswarner/manifest.json

--- custom_components/eiswarner/__init_.py
+++ custom_components/eiswarner/__init_.py
@@ -1,1 +1,13 @@
 """Eiswarner Intregatoin"""
+import asyncio
+from const import DOMAIN
+
+
+async def async_setup(hass, config):
+    api_key = config[DOMAIN]["api_key"]
+    longitude = config[DOMAIN]["longitude"]
+    latitude = config[DOMAIN]["latitude"]
+
+    # Hier können Sie Ihre Add-on-Initialisierung mit den Konfigurationsdaten durchführen
+
+    return True
