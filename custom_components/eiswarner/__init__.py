1f5b7afa05496a0496a6bdb134892fa2473ba90c
seebaer1976 <seebaer1976@web.de>
2023-12-13 20:52:27 +0100

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	custom_components/eiswarner/manifest.json

--- custom_components/eiswarner/__init__.py
+++ custom_components/eiswarner/__init__.py
@@ -1,1 +1,21 @@
 """Eiswarner"""
+from __future__ import annotations
+
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+
+DOMAIN = 'eiswarner'
+
+
+def setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    api_key = config.get("api_key")
+    latitude = config.get("latitude")
+    longitude = config.get("longitude")
+
+    hass.helpers.discovery.load_platform('sensor', DOMAIN, {
+        "api_key": api_key,
+        "latitude": latitude,
+        "longitude": longitude
+    }, config)
+
+    return True
