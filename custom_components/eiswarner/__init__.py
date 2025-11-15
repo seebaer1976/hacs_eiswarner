30d0e19b20539f901976da368d4814d38e07a698
Markus <seebaer1976@web.de>
2025-11-15 07:25:14 +0100

    Refactor Eiswarner setup to use async methods

--- custom_components/eiswarner/__init__.py
+++ custom_components/eiswarner/__init__.py
@@ -1,21 +1,21 @@
-"""Eiswarner"""
-from __future__ import annotations
-
+"""Eiswarner Integration."""
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.typing import ConfigType
 
-DOMAIN = 'eiswarner'
+from .const import DOMAIN
 
+PLATFORMS = ["sensor", "switch"]
 
-def setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    api_key = config.get("api_key")
-    latitude = config.get("latitude")
-    longitude = config.get("longitude")
-
-    hass.helpers.discovery.load_platform('sensor', DOMAIN, {
-        "api_key": api_key,
-        "latitude": latitude,
-        "longitude": longitude
-    }, config)
-
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up Eiswarner from config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = {}
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload Eiswarner config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
