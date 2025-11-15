e9069e58383f81cb66b3d8b24775f0799081b155
Markus <seebaer1976@web.de>
2025-11-15 07:24:17 +0100

    Add constants for Eiswarner component

---
+++ custom_components/eiswarner/const.py
@@ -0,0 +1,9 @@
+"""Constants for Eiswarner."""
+DOMAIN = "eiswarner"
+
+CONF_WEATHER_ENTITY = "weather_entity"
+CONF_FROST_THRESHOLD = "frost_threshold"
+CONF_HUMIDITY_THRESHOLD = "humidity_threshold"
+
+DEFAULT_FROST_THRESHOLD = 0.0
+DEFAULT_HUMIDITY_THRESHOLD = 80.0
