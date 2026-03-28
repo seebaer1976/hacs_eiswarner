768b91711401c62a83782f06f187b6b0df53b1c8
Markus <seebaer1976@web.de>
2026-03-28 12:05:28 +0100

    Update const.py

--- custom_components/eiswarner/const.py
+++ custom_components/eiswarner/const.py
@@ -1,15 +1,29 @@
 """Constants for Eiswarner."""
+
 DOMAIN = "eiswarner"
 
+# Config entry keys
 CONF_API_KEY = "api_key"
 CONF_USE_HA_GEO = "use_ha_geo"
 CONF_LATITUDE = "latitude"
 CONF_LONGITUDE = "longitude"
-CONF_FROST_THRESHOLD = "frost_threshold"
-CONF_HUMIDITY_THRESHOLD = "humidity_threshold"
 
-DEFAULT_FROST_THRESHOLD = 0.0
-DEFAULT_HUMIDITY_THRESHOLD = 80.0
+# Options keys
+CONF_SCAN_INTERVAL = "scan_interval"
 
-API_BASE = "https://api.eiswarnung.de/"
-API_ENDPOINT = "v1/forecast"  # Angenommenes Endpoint basierend auf Docs; passe an wenn nötig
+# Defaults
+DEFAULT_SCAN_INTERVAL = 1800  # 30 Minuten
+
+# API
+API_URL = "https://api.eiswarnung.de/"
+
+# forecastId values
+FORECAST_NO_ICE = 0
+FORECAST_ICE = 1
+FORECAST_MAYBE_ICE = 2
+
+FORECAST_ID_TO_TEXT = {
+    FORECAST_NO_ICE: "Kein Eis",
+    FORECAST_ICE: "Eis",
+    FORECAST_MAYBE_ICE: "Eventuell Eis",
+}
